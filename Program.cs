c1472d3c75662e159e40edc897979dca2ec6bf4d
Yunus Yeşilçimen <135322317+esbyunus@users.noreply.github.com>
2024-03-03 22:19:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.1428;
+            double area, r;
+
+            Console.WriteLine("r = ");
+            r = Convert.ToDouble(Console.ReadLine());
+
+            area = pi * r * r;
+            Console.WriteLine($"for r = {r}, area={area} ");
+            Console.Read();
+        }
+    }
+}
